5e51bc80992a27951aa4fae1e3ed8378f9984d18
Carol Moreira <caahrolmoreira@gmail.com>
2023-03-14 17:19:09 -0300

    addChakraTheme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import {  } from "./App.css";
 import { GlobalProvider } from './contexts/globalcontext';
 import { Router } from "./routes/Router"
 import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from "./theme/theme";
 
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <GlobalProvider>
         <Router />
       </GlobalProvider>
---
+++ src/theme/theme.js
@@ -0,0 +1,27 @@
+import { extendTheme } from "@chakra-ui/react";
+import WallPaper from "../Images/wallpaper-pokeball.png"
+import AshCursor from "../Icons/ash_cursor.png"
+
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bgImage: `url(${WallPaper})`,
+        bgAttachment: 'fixed',
+        cursor: `url(${AshCursor}), auto`
+      },
+        h2: {
+          fontFamily: 'Inter',
+          fontSize: '32px',
+          fontWeight: 700,
+          lineHeight: '38px',
+          color: 'white',
+        },
+      
+      p: {
+        color: 'white',
+      }
+    }
+  }
+})
